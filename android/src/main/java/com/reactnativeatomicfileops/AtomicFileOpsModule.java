376f628e7c3bda6d986eeaa7561f1db01c0339a7
Carl Brown <carlb@pdagent.com>
2020-11-09 18:02:04 -0600

    writeFile() for JS/Android Still needs tests

--- android/src/main/java/com/reactnativeatomicfileops/AtomicFileOpsModule.java
+++ android/src/main/java/com/reactnativeatomicfileops/AtomicFileOpsModule.java
@@ -1,10 +1,19 @@
 package com.reactnativeatomicfileops;
 
+import android.util.Base64;
+
+import androidx.core.util.AtomicFile;
+
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.ReadableMap;
+
+import java.io.File;
+import java.io.FileOutputStream;
+
 
 public class AtomicFileOpsModule extends ReactContextBaseJavaModule {
 
@@ -24,5 +33,23 @@ public class AtomicFileOpsModule extends ReactContextBaseJavaModule {
     public void multiply(int a, int b, Promise promise) {
         // TODO: Implement some actually useful functionality
         promise.resolve(a * b);
+    }
+
+    @ReactMethod
+    public void writeFile(String filePath, String base64Data, ReadableMap options, Promise promise) {
+        try {
+            byte[] data = Base64.decode(base64Data, Base64.DEFAULT);
+
+            File file = new File(filePath);
+            AtomicFile af = new AtomicFile(file);
+            FileOutputStream fos = af.startWrite();
+            fos.write(data);
+            af.finishWrite(fos);
+
+            promise.resolve(null);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            promise.reject(filePath, ex);
+        }
     }
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,11 @@ import { NativeModules } from 'react-native';
 
 type AtomicFileOpsType = {
   multiply(a: number, b: number): Promise<number>;
+  writeFile(
+    filePath: string,
+    data: string,
+    options?: any
+  ): Promise<void>
 };
 
 const { AtomicFileOps } = NativeModules;
