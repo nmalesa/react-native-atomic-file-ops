1be3828fdc6cf259e725a9476ac53ecaa6c5a890
Natalia Malesa <natalia@onlinemeded.org>
2020-11-13 15:18:55 -0600

    Create destination directory URL and file URL

--- ios/AtomicFileOps.swift
+++ ios/AtomicFileOps.swift
@@ -2,7 +2,16 @@
 class AtomicFileOps: NSObject {
 
     @objc(multiply:withB:withResolver:withRejecter:)
-    func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+    func (a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+        
+        let directoryURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+        
+        let fileURL = URL(fileURLWithPath: "Test Message", relativeTo: directoryURL).appendingPathExtension("txt")
+        
+        let myContent = "This is a test."
+        
+        let data = myContent.data(using: .utf8)
+        
         AtomicFileHandler.multiplyAsync(a: a, b: b) { (retVal) in
             resolve(retVal)
         }
