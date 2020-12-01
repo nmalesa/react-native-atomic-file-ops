2108e4569b96c3296df8b4279b06940b8677fc24
Carl Brown <carlb@pdagent.com>
2020-11-30 18:55:35 -0600

    Maintaining string state

--- example/specs/atomicFileWritingSpec.js
+++ example/specs/atomicFileWritingSpec.js
@@ -4,21 +4,10 @@ export default function (spec) {
   spec.describe('Atomic File Writing', function () {
     spec.it('works', async function () {
       await spec.exists('AtomicFileWriterWrapper');
-      await spec.fillIn('WrapperFuncComponent.TextInput', '[{"key": "value"}]');
-      const element = await spec.findComponent('WrapperFuncComponent.TextInput');
+      await spec.fillIn('AtomicFileWriterWrapper', 'This is a Text String');
+      const element = await spec.findComponent('AtomicFileWriterWrapper');
 
-      var seen = [];
-
-      console.log("Element retVal is: "  + JSON.stringify(element.props, function(key, val) {
-        if (val != null && typeof val == "object") {
-          if (seen.indexOf(val) >= 0) {
-            return;
-          }
-          seen.push(val);
-        }
-        return val;
-      }));
-      await hasValue(element,'[{"key": "value"}]');
+      await hasValue(element,'This is a Text String');
     });
   });
 
--- example/specs/fileWritingTestHelper.js
+++ example/specs/fileWritingTestHelper.js
@@ -1,11 +1,10 @@
 import React, { useState } from 'react';
 import { useCavy, wrap, hook } from 'cavy';
-import { View, TextInput } from 'react-native';
+import { Text } from 'react-native';
 
 export default function FileWritingTestHelper(props) {
   const [value, setValue] = useState('');
   const generateTestHook = useCavy();
-  const TestableTextInput = wrap(TextInput);
 
   const onChangeText = (value) => {
     console.log("FileWritingTestHelper: Changed value to: '" + value + "'");
@@ -13,41 +12,24 @@ export default function FileWritingTestHelper(props) {
   };
 
   return (
-    <View>
       <TestableWrapperFuncComponent
         {...props}
-        onChangeText={(value) => {this.setState({value}); }}
+        onChangeText={(value) => { onChangeText({value}); }}
+        value={value}
         ref={generateTestHook('AtomicFileWriterWrapper')}
       />
-    </View>
   );
 }
 
 class WrapperFuncComponent extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { retVal: props.retVal, value: '' };
-    this.onChangeText = this.onChangeText.bind(this);
-    this.props.onChangeText = this.onChangeText;
-  }
-
-  onChangeText(value) {
-    console.log("WrapperFuncComponent: Changed value to: '" + value + "'");
-    this.setState({ value: value });
+    this.state = { retVal: props.retVal, value: props.value };
   }
 
   render() {
-    const { generateTestHook } = this.props;
-    const TestableTextInput = wrap(TextInput);
-
     return (
-      <TestableTextInput
-        ref={generateTestHook('WrapperFuncComponent.TextInput')}
-        onChangeText={this.onChangeText}
-        value={this.state.value}
-      >
-        {this.state.retVal}
-      </TestableTextInput>
+      <Text />
     );
   }
 }
@@ -55,7 +37,19 @@ class WrapperFuncComponent extends React.Component {
 const TestableWrapperFuncComponent = hook(WrapperFuncComponent);
 
 export async function hasValue(element, expected) {
-  const actual = element.props.value;
+  const actual = element.props.value['value'];
+  var seen = [];
+
+  console.log("Actual value is: "  + JSON.stringify(actual, function(key, val) {
+    if (val != null && typeof val == "object") {
+      if (seen.indexOf(val) >= 0) {
+        return;
+      }
+      seen.push(val);
+    }
+    return val;
+  }));
+
   if (actual != expected) {
     throw new Error(`Element text is ${actual}, not ${expected}`);
   }
