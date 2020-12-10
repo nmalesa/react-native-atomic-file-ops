b04d2827faa914534860fee6742f08a68a2bea21
Natalia Malesa <natalia@onlinemeded.org>
2020-12-10 11:10:57 -0600

    Complete createURLs function on AtomicFileOps

--- ios/AtomicFileOps.swift
+++ ios/AtomicFileOps.swift
@@ -3,23 +3,46 @@ import Foundation
 @objc(AtomicFileOps)
 class AtomicFileOps: NSObject {
 
+    // VERSION 1:  Writing atomic file functionality ONLY 
     @objc(multiply:withB:withResolver:withRejecter:)
     func createURLs(fileName: String, fileContent: String, resolve: RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock) -> Void {
         
-//        let directoryURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
-//
-//        let fileURL = URL(fileURLWithPath: fileName, relativeTo: directoryURL).appendingPathExtension("txt")
-//
-//        let content = fileContent
-//
-//        let data = content.data(using: .utf8) // Should this be a guard statement?
-        // Make sure tests past, THEN:
-        // HERE:
-        // If error is not nil, reject with error
-        // If string is not nil, success with string
+        let url = URL(fileURLWithPath: fileName, relativeTo: FileManager.documentDirectoryURL).appendingPathExtension("txt")
+
+        guard let data = fileContent.data(using: .utf8) else {
+            reject(nil, error)
+            return
+        }
         
-        AtomicFileHandler.saveData(fileURL: fileURL, data: data) { (retVal) in
+        AtomicFileHandler.saveData(fileURL: url, data: data) { (retVal) in
             resolve(retVal)
         }
     }
 }
+
+
+
+//public static func saveData(api: String, filePath: String, completionHandler:@escaping (String?, Error?) -> Void) -> Void {
+//    let session: URLSession = URLSession(configuration: .default)
+//
+//    guard let url = URL(string: api) else {
+//        completionHandler(nil, URLError(.badURL))
+//        return
+//    }
+//
+//    session.dataTask(with: url) {data, response, error in
+//            if error != nil {
+//                completionHandler(nil, error)
+//                return
+//            }
+//
+//            guard let data = data else {
+//                completionHandler(nil, URLError(.badServerResponse))
+//                return
+//            }
+//
+//            let output = writeAtomicFile(filePath: filePath, data: data)
+//
+//            completionHandler(output, nil)
+//    }
+//}
