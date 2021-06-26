de865f414f53dce5495729abcbc58efa3455b0b5
Natalia Malesa <natalia@onlinemeded.org>
2021-06-26 15:02:40 -0500

    Update testOverwriteJSON with expectations and file paths

--- ios/AtomicFileOperations/AtomicFileOperationsTests/AtomicFileOperationsTests.swift
+++ ios/AtomicFileOperations/AtomicFileOperationsTests/AtomicFileOperationsTests.swift
@@ -29,9 +29,9 @@ class AtomicFileOperationsTests: XCTestCase {
     if FileManager.default.fileExists(atPath: filePath) {
       try FileManager.default.removeItem(atPath: filePath)
     }
-  
     XCTAssertFalse(FileManager.default.fileExists(atPath: filePath))
     
+    // Set expectation to verify asynchronous writing behaves as expected
     let expectation = self.expectation(description: "File written.")
         
     // Write out the full file, and read the file back in 
@@ -40,18 +40,17 @@ class AtomicFileOperationsTests: XCTestCase {
       if let existingError = error { // Could be guard statement alternatively (idiomatic Swift)
         XCTFail(existingError.localizedDescription)
       }
-      let fileExists: Bool = FileManager.default.fileExists(atPath: filePath)
-      XCTAssertTrue(fileExists)
+      XCTAssertTrue(FileManager.default.fileExists(atPath: filePath))
       expectation.fulfill()
     }
 
-    // Clean up   // RUNNING TOO EARLY - THINK ASYNC
     waitForExpectations(timeout: 10) { error in
       if let existingError = error { // Could be guard statement alternatively (idiomatic Swift)
         XCTFail(existingError.localizedDescription)
       }
     }
     
+    // Clean up
     if FileManager.default.fileExists(atPath: filePath) {
       try FileManager.default.removeItem(atPath: filePath)
     }
@@ -61,28 +60,49 @@ class AtomicFileOperationsTests: XCTestCase {
     let jsonString: String = "[{\"key\": \"value\"}]"
     let time = Date().timeIntervalSince1970
     let timeString = String(time).replacingOccurrences(of: ".", with: "_")
-    let filePath: String = "AtomicFileOpsModuleTest.test." + timeString
+    let fileName: String = "AtomicFileOpsModuleTest.test." + timeString + ".json"
+    let directory = FileManager.documentDirectoryURL
+    let filePath = directory.appendingPathComponent(fileName).path
       
     // Make sure file does not already exist
-    let fileExists: Bool = FileManager.default.fileExists(atPath: filePath) // Need absolute path?
-    
-    if fileExists {
+    if FileManager.default.fileExists(atPath: filePath) {
       try FileManager.default.removeItem(atPath: filePath)
-      XCTAssertEqual(false, fileExists)
     }
+    XCTAssertFalse(FileManager.default.fileExists(atPath: filePath))
+    
+    // Set expectation to verify asynchronous writing behaves as expected
+    let expectation = self.expectation(description: "File written.")
       
     // Write out the full file, and read the file back in
-    AtomicFileHandler.writeFile(fileName: filePath, contents: jsonString, characterSet: .utf8) { (retVal, error) in
+    AtomicFileHandler.writeFile(fileName: fileName, contents: jsonString, characterSet: .utf8, directory: directory.path) { (retVal, error) in
       XCTAssertEqual("[{\"key\": \"value\"}]", retVal)
+      if let existingError = error { // Could be guard statement alternatively (idiomatic Swift)
+        XCTFail(existingError.localizedDescription)
+      }
+      XCTAssertTrue(FileManager.default.fileExists(atPath: filePath))
+      expectation.fulfill()
     }
-      
+    
+    let overwriteExpectation = self.expectation(description: "File overwritten.")
+
     // Overwrite the file with a shorter string, and read the file back in.
-    AtomicFileHandler.writeFile(fileName: filePath, contents: "[]", characterSet: .utf8) { (retVal, error) in
+    AtomicFileHandler.writeFile(fileName: fileName, contents: "[]", characterSet: .utf8) { (retVal, error) in
       XCTAssertEqual("[]", retVal)
+      if let existingError = error { // Could be guard statement alternatively (idiomatic Swift)
+        XCTFail(existingError.localizedDescription)
+      }
+      XCTAssertTrue(FileManager.default.fileExists(atPath: filePath))
+      overwriteExpectation.fulfill()
+    }
+    
+    waitForExpectations(timeout: 10) { error in
+      if let existingError = error { // Could be guard statement alternatively (idiomatic Swift)
+        XCTFail(existingError.localizedDescription)
+      }
     }
   
     // Clean up
-    if fileExists {
+    if FileManager.default.fileExists(atPath: filePath) {
       try FileManager.default.removeItem(atPath: filePath)
     }
   }
