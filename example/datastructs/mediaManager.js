1bc920bd60f14f633c2959a70026021918298ecd
Natalia Malesa <natalia@onlinemeded.org>
2021-06-30 10:37:25 -0500

    Add media manager and update Cavy specs

---
+++ example/datastructs/mediaManager.js
@@ -0,0 +1,231 @@
+import { Platform } from 'react-native';
+import RNFS from 'react-native-fs';
+import AtomicFileOps from 'react-native-atomic-file-ops'
+import _ from 'lodash';
+
+export const writeFile = async content => {
+  const filePath = RNFS.DocumentDirectoryPath + '/cavyImageCaption.txt';
+  try {
+    await RNFS.writeFile(filePath, content, 'utf8');
+    console.log('FILE WRITTEN!');
+  } catch (error) {
+    console.log('writeFile: ', error);
+  }
+};
+
+export const readFile = async (path, encoding) => {
+  try {
+    if (!path || typeof path !== 'string') {
+      throw new Error('readFile: Path must be a string.')
+    }
+    
+    const fileExists = await RNFS.exists(path)
+    if (!fileExists) {
+      throw new Error('readFile: File does not exist.')
+    }
+
+    const data = await RNFS.readFile(path, encoding)
+    // console.log('Data: ', data)
+    return data
+  } catch (error) {
+    console.error('readFile: ', error)
+  }
+}
+
+export const deleteFile = async path => {
+  try {
+    await RNFS.unlink(path);
+    console.log('FILE DELETED!');
+  } catch (error) {
+    console.log('deleteFile: ', error);
+  }
+};
+
+export const hasFreeStorage = async () => {
+  const minFreeStorageBytes = 500 * 10 ** 6;
+  const diskInfo = await RNFS.getFSInfo();
+  return _.get(diskInfo, 'freeSpace', 0) > minFreeStorageBytes;
+};
+
+export const downloadExternalFile = async config => {
+  try {
+    const fileName = _.get(config, 'fileName', null);
+    const headers = _.get(config, 'headers', {});
+    const url = _.get(config, 'url', null);
+
+    if (!fileName || !url) {
+      const error = new Error('downloadExternalFile: Missing parameters');
+      error.internal = true;
+      throw error;
+    }
+
+    let directory = _.get(RNFS, 'DocumentDirectoryPath', null);
+    let filePath = `${directory}/${fileName}`;
+
+    // THROWING ERROR:  HASFREESTORAGE IS NOT A FUNCTION
+    // const hasFreeStorage = await hasFreeStorage();
+    // if (!hasFreeStorage) {
+    //   throw new Error('Error.  Phone storage is almost full.');
+    // }
+
+    const dlResults = await RNFS.downloadFile({
+      headers,
+      fromUrl: encodeURI(url), // URL to download file from
+      toFile: `${filePath}`, // Local filesystem path to save the file to
+      background: true, // Continue the download in the background after the app terminates (iOS only)
+      discretionary: true, // Allow the OS to control the timing and speed of the download to improve perceived performance  (iOS only)
+      cacheable: false, // Whether the download can be stored in the shared NSURLCache (iOS only, defaults to true)
+    }).promise;
+
+    if (Platform.OS === 'ios') {
+      RNFS.completeHandlerIOS(dlResults.jobId);
+    }
+
+    const statusCode = _.get(dlResults, 'statusCode', 400);
+    if (statusCode < 200 || statusCode >= 400) {
+      throw new Error(
+        'There was an error downloading this external file.  Please try again later.',
+      );
+    }
+    console.log('Download external file.');
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const fetchHTTPFile = async url => {
+  try {
+    const fileName = url.split('/').pop();
+
+    if (!fileName) {
+      const error = new Error('getPathforContentFile: Missing file name');
+      error.internal = true;
+      throw error;
+    }
+
+    let directory = _.get(RNFS, 'DocumentDirectoryPath', null);
+    // let filePath = `${directory}/${fileName}`;
+
+    if (!directory) {
+      await RNFS.mkdir(directory);
+    }
+
+    await downloadExternalFile({
+      fileName,
+      url,
+    });
+  } catch (error) {
+    console.error('Error: ', error);
+  }
+};
+
+export const getPathOfFetchedHTTPFile = async url => {
+  if (!url) {
+    const error = new Error('pathOfFetchedHTTPFile: Missing URL');
+    error.internal = true;
+    throw error;
+  }
+
+  try {
+    const fileName = url.split('/').pop();
+    const directory = _.get(RNFS, 'DocumentDirectoryPath', null);
+
+    if (!directory) {
+      await RNFS.mkdir(directory);
+    }
+
+    const filePath = `${directory}/${fileName}`;
+    // console.log('getPathOfFetchedHTTPFile File Path: ', filePath);
+    return filePath;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const deleteFetchedHTTPFile = async url => {
+  try {
+    const fileName = url.split('/').pop();
+    const filePath = RNFS.DocumentDirectoryPath + `/${fileName}`;
+    return await RNFS.unlink(filePath);
+  } catch (error) {
+    console.log('deleteFetchedHTTPFile: ', error);
+  }
+};
+
+export const getImageFileList = async () => {
+  try {
+    return await readImageDir();
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
+
+export const readImageDir = async () => {
+  let directory = _.get(RNFS, 'DocumentDirectoryPath', null);
+  let imageDir = `${directory}/image`
+
+  let imageDirExists = null
+
+  try {
+    imageDirExists = await RNFS.exists(imageDir)
+  } catch (error) {
+    imageDirExists = false
+  }
+
+  const imageMetaPath = `${imageDir}/meta.json`
+
+  let imageMetaExists = null
+  try {
+    imageMetaExists = await RNFS.exists(imageMetaPath)
+  } catch (error) {
+    imageMetaExists = false
+  }
+ 
+  if (!imageMetaExists) {
+    await RNFS.mkdir(imageDir)
+    await AtomicFileOps.writeFile(imageMetaPath, JSON.stringify([]), 'utf8')
+  }
+
+  const metaData = await parseMetaFile(imageMetaPath)
+
+  console.log('Metadata: ', metaData)
+  return metaData
+};
+
+export const parseMetaFile = async metaPath => {
+  const metaJSON = await readFile(metaPath, 'utf8')
+  let meta = null
+  try {
+    meta = JSON.parse(metaJSON)
+  } catch {
+    meta = []
+  }
+    /*
+    check the meta file integrity by looking that all the files in this dir
+    are present, if not remove them from meta.
+  */
+  let errors = 0
+  const checkedMeta = []
+  for (let i = 0; i < meta.length; i++) {
+    const row = meta[i]
+    const filePath = `${row.path}/${row.filename}`
+    try {
+      const fileExist = await RNFS.exists(filePath)
+      if (fileExist) {
+        checkedMeta.push(row)
+      } else {
+        errors += 1
+      }
+    } catch (e) {
+      errors += 1
+    }
+  }
+
+  if (errors) {
+    // overwrite meta file
+    await AtomicFileOps.writeFile(metaPath, JSON.stringify(checkedMeta), 'utf8')
+  }
+
+  return checkedMeta
+}
--- example/specs/atomicFileWritingSpec.js
+++ example/specs/atomicFileWritingSpec.js
@@ -1,15 +1,109 @@
-import { hasValue } from './fileWritingTestHelper';
+// import { hasValue } from './fileWritingTestHelper';
+
+// export default function (spec) {
+//   spec.describe('Atomic File Writing', function () {
+//     spec.it('works', async function () {
+//       await spec.exists('AtomicFileWriterWrapper');
+//       await spec.fillIn('AtomicFileWriterWrapper', 'This is a Text String');
+//       const element = await spec.findComponent('AtomicFileWriterWrapper');
+
+//       await hasValue(element,'This is a Text String');
+//     });
+//   });
+
+// }
+
+import RNFS from 'react-native-fs';
+import AtomicFileOps from 'react-native-atomic-file-ops'
+import _ from 'lodash';
+import {
+  readFile,
+  getPathOfFetchedHTTPFile,
+  deleteFetchedHTTPFile,
+  fetchHTTPFile,
+  getImageFileList
+} from '../datastructs/mediaManager';
+
+const REMOTE_FILE_PATH =
+  'https://live.staticflickr.com/7377/26722155994_5200abc340_b.jpg';
+
+// Length = 147 characters
+const IMAGE_1 = [
+  {
+    alt: 'Sasu the Guinea Pig',
+    creator: 'andymiccone',
+    url: 'https://live.staticflickr.com/7377/26722155994_5200abc340_b.jpg',
+    license: 'CC0 1.0'
+  }
+]
+
+// Length = 156 characters
+const IMAGE_2 = [
+  {
+    alt: 'white and brown guinea pig on brown wood photo',
+    creator: 'Amjith S',
+    url: 'https://unsplash.com/photos/6p0Lym68MSI',
+    license: 'Unsplash License'
+  }
+]
 
 export default function (spec) {
-  spec.describe('Atomic File Writing', function () {
-    spec.it('works', async function () {
-      await spec.exists('AtomicFileWriterWrapper');
-      await spec.fillIn('AtomicFileWriterWrapper', 'This is a Text String');
-      const element = await spec.findComponent('AtomicFileWriterWrapper');
+  spec.describe('tests file system', function () {
+    spec.it('fetches', async function () {
+      const filePath = await getPathOfFetchedHTTPFile(REMOTE_FILE_PATH);
 
-      await hasValue(element,'This is a Text String');
+      if (await RNFS.exists(filePath)) {
+        await deleteFetchedHTTPFile(REMOTE_FILE_PATH);
+      }
+
+      const fileExists = await RNFS.exists(filePath);
+  
+      if (fileExists) {
+        throw new Error('File cannot exist before we fetch it.');
+      }
+
+      await fetchHTTPFile(REMOTE_FILE_PATH);
+
+      await spec.pause(2000); // Needs to be long enough that the fetch completes
+
+      const fileExistsNow = await RNFS.exists(filePath);
+
+      console.log('File Exists Now: ', fileExistsNow)
+      // NOTE:  In article, make a note about how the debugger is helpful to pause and see what's being logged
+      if (!fileExistsNow) {
+        throw new Error('File does not exist after we fetched it.');
+      }
     });
-  });
 
+    spec.it('overwrites metadata file', async function () {
+      await AtomicFileOps.writeFile("cavy_test_image.json", JSON.stringify(IMAGE_2), 'utf8');
+
+      await AtomicFileOps.writeFile("cavy_test_image.json", JSON.stringify(IMAGE_1), 'utf8');
+    })
+    
+    // spec.it('fixes corrupted image metadata file', async function () {
+    //   try {
+    //     const emptyList = await getImageFileList()
+    //     if (emptyList.length != 0) {
+    //       throw 'List should be empty.'
+    //     }
+    //   } catch (error) {
+    //     console.log('Error from getImageFileList: ', error);
+    //   }
+
+    //   // Now corrupt the metadata file by writing garbage in it
+    //   let directory = _.get(RNFS, 'DocumentDirectoryPath', null);
+    //   let imageDirPath = `${directory}/image`
+    //   const imageMetaPath = `${imageDirPath}/meta.json`
+
+    //   // await AtomicFileOps.writeFile(imageMetaPath, 'rkP7P3bu;.><5_/V?', 'utf8')
+
+    //   const shouldStillBeEmptyList = await getImageFileList()
+
+    //   if (shouldStillBeEmptyList.length != 0) {
+    //     throw 'List should be empty.'
+    //   }
+    // })
+  })
 }
 
