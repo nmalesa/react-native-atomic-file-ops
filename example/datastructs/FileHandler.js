bddec6c884c073b9fac68fbf94ff3f4faae53bc6
Natalia Malesa <natalia@onlinemeded.org>
2021-07-13 15:40:35 -0500

    Test that fetch functions are working as expected

--- example/datastructs/FileHandler.js
+++ example/datastructs/FileHandler.js
@@ -2,7 +2,114 @@ import RNFS from 'react-native-fs';
 import AtomicFileOps from 'react-native-atomic-file-ops'
 import _ from 'lodash'
 
-export class FileHandler { }
+export class FileHandler { 
+  static deleteFetchedHTTPFile = async url => {
+    try {
+      const fileName = url.split('/').pop();
+      const filePath = RNFS.DocumentDirectoryPath + `/${fileName}`;
+      return await RNFS.unlink(filePath);
+    } catch (error) {
+      console.log('deleteFetchedHTTPFile: ', error);
+    }
+  };
+
+  static hasFreeStorage = async () => {
+    const minFreeStorageBytes = 500 * 10 ** 6;
+    const diskInfo = await RNFS.getFSInfo();
+    return _.get(diskInfo, 'freeSpace', 0) > minFreeStorageBytes;
+  };
+  
+  static downloadExternalFile = async config => {
+    try {
+      const fileName = _.get(config, 'fileName', null);
+      const headers = _.get(config, 'headers', {});
+      const url = _.get(config, 'url', null);
+  
+      if (!fileName || !url) {
+        const error = new Error('downloadExternalFile: Missing parameters');
+        error.internal = true;
+        throw error;
+      }
+  
+      let directory = _.get(RNFS, 'DocumentDirectoryPath', null);
+      let filePath = `${directory}/${fileName}`;
+  
+      const hasFreeStorage = await this.hasFreeStorage();
+      if (!hasFreeStorage) {
+        throw new Error('Error.  Phone storage is almost full.');
+      }
+  
+      const dlResults = await RNFS.downloadFile({
+        headers,
+        fromUrl: encodeURI(url), // URL to download file from
+        toFile: `${filePath}`, // Local filesystem path to save the file to
+        background: true, // Continue the download in the background after the app terminates (iOS only)
+        discretionary: true, // Allow the OS to control the timing and speed of the download to improve perceived performance  (iOS only)
+        cacheable: false, // Whether the download can be stored in the shared NSURLCache (iOS only, defaults to true)
+      }).promise;
+  
+      if (Platform.OS === 'ios') {
+        RNFS.completeHandlerIOS(dlResults.jobId);
+      }
+  
+      const statusCode = _.get(dlResults, 'statusCode', 400);
+      if (statusCode < 200 || statusCode >= 400) {
+        throw new Error(
+          'There was an error downloading this external file.  Please try again later.',
+        );
+      }
+      console.log('Download external file.');
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  static fetchHTTPFile = async url => {
+    try {
+      const fileName = url.split('/').pop();
+  
+      if (!fileName) {
+        const error = new Error('getPathforContentFile: Missing file name');
+        error.internal = true;
+        throw error;
+      }
+  
+      let directory = _.get(RNFS, 'DocumentDirectoryPath', null);
+      // let filePath = `${directory}/${fileName}`;
+  
+      if (!directory) {
+        await RNFS.mkdir(directory);
+      }
+  
+      await this.downloadExternalFile({
+        fileName,
+        url,
+      });
+    } catch (error) {
+      console.error('Error: ', error);
+    }
+  };
+
+  static getPathOfFetchedHTTPFile = async url => {
+    if (!url) {
+      throw new Error('pathOfFetchedHTTPFile: Missing URL');
+    }
+  
+    try {
+      const fileName = url.split('/').pop();
+      const directory = RNFS.DocumentDirectoryPath
+  
+      if (!directory) {
+        await RNFS.mkdir(directory);
+      }
+  
+      const filePath = `${directory}/${fileName}`;
+      return filePath;
+    } catch (error) {
+      console.error(error);
+    }
+  };
+}
 
 
 
--- example/specs/atomicFileWritingSpec.js
+++ example/specs/atomicFileWritingSpec.js
@@ -5,6 +5,7 @@ import { FileHandler } from '../datastructs/FileHandler';
 const fileName = 'CavyTest.json'
 const directory = RNFS.DocumentDirectoryPath
 const filePath = `${directory}/${fileName}`;
+const remoteFilePath = 'https://live.staticflickr.com/7377/26722155994_5200abc340_b.jpg'
 
 
 export default function (spec) {
@@ -16,25 +17,27 @@ export default function (spec) {
       }
     })
 
-    spec.it('overwrites JSON', async function () {
-      // Write the file
-      await AtomicFileOps.writeFile(filePath, "[{\"Guinea pig\": \"Cavia porcellus\"}]", 'UTF8')
+    // spec.it('overwrites JSON', async function () {
+    //   // Write the file
+    //   await AtomicFileOps.writeFile(filePath, "[{\"Guinea pig\": \"Cavia porcellus\"}]", 'UTF8')
 
-      // Overwrite the same file with shorter JSON data
-      await AtomicFileOps.writeFile(filePath, "[{\"Cat\": \"Felis catus\"}]", 'UTF8')
+    //   // Overwrite the same file with shorter JSON data
+    //   await AtomicFileOps.writeFile(filePath, "[{\"Cat\": \"Felis catus\"}]", 'UTF8')
 
-      const content = await RNFS.readFile(filePath, 'utf8') 
+    //   const content = await RNFS.readFile(filePath, 'utf8') 
       
-      if (content !== "[{\"Cat\": \"Felis catus\"}]") {
-        throw 'Overwrites JSON Error:  Content does not match truncated input.'
-      } 
-    })
+    //   if (content !== "[{\"Cat\": \"Felis catus\"}]") {
+    //     throw 'Overwrites JSON Error:  Content does not match truncated input.'
+    //   } 
+    // })
 
     spec.it('handles Base64', async function () {
-      const filePath = await getPathOfFetchedHTTPFile(REMOTE_FILE_PATH);
+      const filePath = await FileHandler.getPathOfFetchedHTTPFile(remoteFilePath);
+
+      console.log(filePath)
 
       if (await RNFS.exists(filePath)) {
-        await deleteFetchedHTTPFile(REMOTE_FILE_PATH);
+        await FileHandler.deleteFetchedHTTPFile(remoteFilePath);
       }
 
       const fileExists = await RNFS.exists(filePath);
@@ -43,24 +46,23 @@ export default function (spec) {
         throw new Error('File cannot exist before we fetch it.');
       }
 
-      await fetchHTTPFile(REMOTE_FILE_PATH);
+      await FileHandler.fetchHTTPFile(remoteFilePath);
 
       await spec.pause(2000); // Needs to be long enough that the fetch completes
 
       const fileExistsNow = await RNFS.exists(filePath);
 
       console.log('File Exists Now: ', fileExistsNow)
+
+      // debugger
       // NOTE:  In article, make a note about how the debugger is helpful to pause and see what's being logged
       if (!fileExistsNow) {
         throw new Error('File does not exist after we fetched it.');
       }
 
-      const content = await readFile(filePath, 'base64')
+      const content = await RNFS.readFile(filePath, 'base64')
 
-      let jsonString = `[{\"uri\": \"${content}\"}]`
-      // console.log('JSON: ', jsonString)
-
-      await AtomicFileOps.writeFile('Base64Test.json', jsonString, 'base64')
+      console.log('Content: ', content)
     });
 
   })
