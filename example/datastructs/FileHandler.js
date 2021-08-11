8420a5de7e8d2d120c0ba2115f7286d4c489313f
Natalia Malesa <natalia@onlinemeded.org>
2021-08-11 12:21:13 -0500

    Overwrites truncated Base64 data

--- example/datastructs/FileHandler.js
+++ example/datastructs/FileHandler.js
@@ -109,6 +109,3 @@ export class FileHandler {
     }
   };
 }
-
-
-
--- example/specs/atomicFileWritingSpec.js
+++ example/specs/atomicFileWritingSpec.js
@@ -1,11 +1,9 @@
 import RNFS from 'react-native-fs';
 import AtomicFileOps from 'react-native-atomic-file-ops'
-import { FileHandler } from '../datastructs/FileHandler';
 
 const fileName = 'CavyTest.json'
 const directory = RNFS.DocumentDirectoryPath
 const filePath = `${directory}/${fileName}`;
-const remoteFilePath = 'https://live.staticflickr.com/7377/26722155994_5200abc340_b.jpg'
 
 
 export default function (spec) {
@@ -17,65 +15,35 @@ export default function (spec) {
       }
     })
 
-    spec.it('tests base64 on Android', async function () {
-      await AtomicFileOps.writeFile(filePath, "Hello, world!", "BASE64")
+    spec.it('overwrites JSON', async function () {
+      // Write the file
+      await AtomicFileOps.writeFile(filePath, "[{\"Guinea pig\": \"Cavia porcellus\"}]", 'UTF8')
+
+      // Overwrite the same file with shorter JSON data
+      await AtomicFileOps.writeFile(filePath, "[{\"Cat\": \"Felis catus\"}]", 'UTF8')
+
+      const content = await RNFS.readFile(filePath, 'utf8') 
+      
+      if (content !== "[{\"Cat\": \"Felis catus\"}]") {
+        throw 'Overwrites JSON Error:  Content does not match truncated input.'
+      } 
     })
 
-    // spec.it('overwrites JSON', async function () {
-    //   // Write the file
-    //   await AtomicFileOps.writeFile(filePath, "[{\"Guinea pig\": \"Cavia porcellus\"}]", 'UTF8')
+    spec.it('handles Base64', async function () {
+      // Write the file
+      await AtomicFileOps.writeFile(filePath, "Guinea pig", 'BASE64')
+      // 'Guinea pig' in Base64:  R3VpbmVhIHBpZw==
+    
+      // Overwrite the same file with shorter data
+      await AtomicFileOps.writeFile(filePath, "Cat", 'BASE64')
 
-    //   // Overwrite the same file with shorter JSON data
-    //   await AtomicFileOps.writeFile(filePath, "[{\"Cat\": \"Felis catus\"}]", 'UTF8')
+      const content = await RNFS.readFile(filePath, 'base64')
+      // 'Cat' in Base64:  Q2F0
 
-    //   const content = await RNFS.readFile(filePath, 'utf8') 
-      
-    //   if (content !== "[{\"Cat\": \"Felis catus\"}]") {
-    //     throw 'Overwrites JSON Error:  Content does not match truncated input.'
-    //   } 
-    // })
-
-    // spec.it('handles Base64', async function () {
-    //   const filePath = await FileHandler.getPathOfFetchedHTTPFile(remoteFilePath);
-
-    //   console.log(filePath)
-
-    //   if (await RNFS.exists(filePath)) {
-    //     await FileHandler.deleteFetchedHTTPFile(remoteFilePath);
-    //   }
-
-    //   const fileExists = await RNFS.exists(filePath);
-  
-    //   if (fileExists) {
-    //     throw new Error('File cannot exist before we fetch it.');
-    //   }
-
-    //   await FileHandler.fetchHTTPFile(remoteFilePath);
-
-    //   await spec.pause(2000); // Needs to be long enough that the fetch completes
-
-    //   // Error:  Error: Unable to resolve host "live.staticflickr.com": No address associated with hostname
-
-    //   const fileExistsNow = await RNFS.exists(filePath);
-
-    //   console.log('File Exists Now: ', fileExistsNow)
-
-    //   debugger
-    //   // NOTE:  In article, make a note about how the debugger is helpful to pause and see what's being logged
-    //   if (!fileExistsNow) {
-    //     throw new Error('File does not exist after we fetched it.');
-    //   }
-
-      // const content = await RNFS.readFile(filePath, 'base64')
-
-      // console.log('Content: ', content)
-
-      // const imageFilePath = RNFS.DocumentDirectoryPath + '/Base64.jpg'
-
-      // await RNFS.writeFile(imageFilePath, content, 'base64')
-      // await AtomicFileOps.writeFile(imageFilePath, content, 'BASE64')
-    // });
-
+      if (content !== 'Q2F0') {
+        throw 'Overwrites Base64 error:  Content does not match truncated input.'
+      }
+    })
   })
 }
 
