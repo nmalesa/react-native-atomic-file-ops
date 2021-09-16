551817ee047fa3e4d13eb41b60fd73b74816b1d9
Natalia Malesa <natalia@onlinemeded.org>
2021-09-16 18:07:05 -0500

    Rename parameters in Swift

--- ios/AtomicFileOps.swift
+++ ios/AtomicFileOps.swift
@@ -3,8 +3,8 @@ import Foundation
 @objc(AtomicFileOps)
 class AtomicFileOps: NSObject {
     @objc(writeFile:withData:withOptions:withResolver:withRejecter:)
-    func writeFile(filePath: String, data: String, options: String, resolve:@escaping RCTPromiseResolveBlock,reject:@escaping RCTPromiseRejectBlock) -> Void {
-      AtomicFileHandler.writeFile(fileName: filePath, contents: data, characterSet: options) { (returnString, error) in
+    func writeFile(fileName: String, contents: String, encoding: String, resolve:@escaping RCTPromiseResolveBlock,reject:@escaping RCTPromiseRejectBlock) -> Void {
+      AtomicFileHandler.writeFile(fileName: fileName, contents: contents, encoding: encoding) { (returnString, error) in
           guard error == nil else {
             reject("Error", error?.localizedDescription, error)
               return
